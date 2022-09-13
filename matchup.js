871dd7fe374aec4f29f97629b2646ddc6441af84
Kirk Pettinga <kirk.pettinga@gmail.com>
2022-09-12 22:49:34 -0500

    use proTeamId to find projection stat

--- matchup.js
+++ matchup.js
@@ -1,6 +1,4 @@
 import axios from 'axios'
-import { google } from 'googleapis';
-// import { authorize } from './auth.js';
 
 export const fetchMatchupResults = async (leagueId) => {
 
@@ -67,14 +65,16 @@ export const fetchMatchupResults = async (leagueId) => {
                 const { lineupSlotId, playerPoolEntry: { player } } = rosterEntry
                 // only add up players with slot IDs 0 to 8. The rest are on the bench.
                 if ( isStarter(lineupSlotId) ) {
-                    awayProjectedPoints += player.stats[player.stats.length - 1].appliedTotal
+                    let stat = player.stats.find( s => s.proTeamId === 0 )
+                    awayProjectedPoints += stat.appliedTotal
                 }
             } )
             matchup.home.rosterForCurrentScoringPeriod.entries.forEach( rosterEntry => {
                 const { lineupSlotId, playerPoolEntry: { player } } = rosterEntry
                 // only add up players with slot IDs 0 to 8. The rest are on the bench.
                 if ( isStarter(lineupSlotId) ) {
-                    homeProjectedPoints += player.stats[player.stats.length - 1].appliedTotal
+                    let stat = player.stats.find( s => s.proTeamId === 0 )
+                    homeProjectedPoints += stat.appliedTotal
                 }
             } )
     
@@ -142,32 +142,3 @@ export const fetchMatchupResults = async (leagueId) => {
     }
 
 }
-
-// fetchMatchupResults(2134550616).then( results => console.log(results) )
-
-// authorize()
-//     .then(listMajors)
-//     .catch(console.error);
-
-// /**
-//  * Prints the names and majors of students in a sample spreadsheet:
-//  * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-//  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
-//  */
-//  async function listMajors(auth) {
-//     const sheets = google.sheets({ version: 'v4', auth });
-//     const res = await sheets.spreadsheets.values.get({
-//         spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-//         range: 'Class Data!A2:E',
-//     });
-//     const rows = res.data.values;
-//     if (!rows || rows.length === 0) {
-//         console.log('No data found.');
-//         return;
-//     }
-//     console.log('Name, Major:');
-//     rows.forEach((row) => {
-//         // Print columns A and E, which correspond to indices 0 and 4.
-//         console.log(`${row[0]}, ${row[4]}`);
-//     });
-// }
